c6adb5d398793949125fbc4ae73d5799863c981c
Guido <guido10md@gmail.com>
2021-05-20 14:09:43 -0300

    Added categories controller with authentication (doesn't filter by user)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_constraints.rb'
 
 Rails.application.routes.draw do
+  resources :categories
   namespace :api, default: {format: :json} do
     
     # This sets the authentication route for devise
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
       resources :posts
+      resources :categories
     end
   end
 end
